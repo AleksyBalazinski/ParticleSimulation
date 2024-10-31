b0588252b573fb01f3d029dc9f535ce62d64734b
AleksyBalazinski <aleksy.j.balazinski@gmail.com>
2024-10-31 20:47:29 +0100

    Add project files.

---
+++ ParticleSimulation/ParticleSimulation.cpp
@@ -0,0 +1,12 @@
+﻿// ParticleSimulation.cpp : Defines the entry point for the application.
+//
+
+#include "ParticleSimulation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
