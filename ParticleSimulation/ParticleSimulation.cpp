8977561053182085bfc0e1a3abe5678e0f8d057f
AleksyBalazinski <aleksy.j.balazinski@gmail.com>
2024-11-05 13:45:31 +0100

    add RK4

--- ParticleSimulation/ParticleSimulation.cpp
+++ ParticleSimulation/ParticleSimulation.cpp
@@ -1,12 +1,23 @@
-﻿// ParticleSimulation.cpp : Defines the entry point for the application.
-//
+﻿#include <iostream>
+#include "RK4Stepper.h"
 
-#include "ParticleSimulation.h"
-
-using namespace std;
+void harmonicOscillator(std::array<double, 2>& x, std::array<double, 2>& dxdt, double t) {
+	dxdt[0] = x[1];
+	dxdt[1] = -x[0];
+}
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	std::array<double, 2> x0 = { 3, 0 };
+	RK4Stepper<double, 2> stepper(harmonicOscillator, x0);
+
+	double stopT = 151 * 3.141;
+	double dt = 0.0001;
+	int stepsCnt = (int)(stopT / dt);
+	for (int i = 0; i < stepsCnt; i++) {
+		stepper.doStep(dt);
+	}
+	
+	auto& x = stepper.getX();
+	std::cout << x[0] << ' ' << x[1] << '\n';
 }
